fc66a6ec2400bab96ccffc0fcc535c4c222d33d9
Maria <mariia.v.litvinova@gmail.com>
2017-02-07 18:38:24 +0300

    added generation of theme and full name lines from table

--- generateBachelorsThesisLinks.py
+++ generateBachelorsThesisLinks.py
@@ -3,19 +3,24 @@
 # Tool for generating content of diploma works index for Software Engineering chair of St. Petersburg State University.
 # See http://se.math.spbu.ru/SE/YearlyProjects/2012/list for example of output and https://www.dropbox.com/sh/ob7g6zeur45mgol/1x9riSzSXV for example of possible input.
 # Usage: copy this script into a directory with semester works and run it. Output will appear in output.txt.
+# This script needs file 'table.csv' in the same directory (the first column must be full name of student, second column - student surname in english, fourth column - thesis theme).
+# Surname in english has to be the same as surname in title of files with thesis, review, presentation etc.
+# If there is no such file, output.txt will be generated, but without full name and theme lines.
 
 import os
 import re
+import csv
 
 year = 2015
 
 class Student(object):
 
 	__template = "    <tr class=\"%s\">\n" \
-		"      <td>  </td>\n" \
-		"      <td>  </td>\n" \
-		"%s" \
-		"    </tr>\n\n"
+				 "%s" \
+				 "    </tr>\n\n"
+
+	__titleTemplate = "      <td> %s </td>\n"
+	__emptyTitleTemplate = "      <td>  </td>\n"
 
 	__fileTemplate = "      <td><a href=\"bmo/%s\">%s</a></td>\n"
 	__reportTemplate = "Текст"
@@ -24,13 +29,15 @@ class Student(object):
 	__advisorReviewTemplate = "Отзыв научного руководителя"
 	__reviewerReviewTemplate = "Отзыв рецензента"
 
-	def __init__(self, name):
+	def __init__(self, name, fullName, theme):
 		self.presentation = ""
 		self.report = ""
 		self.sources = ""
 		self.advisorReview = ""
 		self.reviewerReview = ""
 		self.name = name
+		self.fullName = fullName
+		self.theme = theme
 	
 	def __str__(self):
 		return "Name: " + self.name + ", report: " + self.report + ", presentation: " \
@@ -57,6 +64,17 @@ class Student(object):
 		if docClass == 0:
 			documentClass = "even"	
 		body = ""
+
+		if self.fullName != "":
+			body += self.__titleTemplate % (self.fullName)
+		else:
+			body += self.__emptyTitleTemplate
+
+		if self.theme != "":
+			body += self.__titleTemplate % (self.theme)
+		else:
+			body += self.__emptyTitleTemplate
+
 		if self.report != "":
 			body += self.__fileTemplate % (self.report, self.__reportTemplate)
 		else:
@@ -112,13 +130,51 @@ def find(predicate, list):
 			return item
 	return None
 
+
+def addValueToDictionary(d, key, value):
+	if key not in d and key:
+		d[key] = value
+
+
+def getValueFromDictionary(d, key):
+	value = ""
+	if key in d:
+		value = d[key]
+
+	return value
+
+
+def initDictionaries(csvFile):
+	fullNames = {}
+	themes = {}
+	with open(csvFile, 'rt', encoding='utf-8') as file:
+		csvReader = csv.reader(file)
+		for row in csvReader:
+			key = row[1]
+			fullName = row[0]
+			theme = row[3]
+
+			addValueToDictionary(fullNames, key, fullName)
+			addValueToDictionary(themes, key, theme)
+	return fullNames, themes
+
+
 files = os.listdir(".")
+
+fullNames, themes = {}, {}
+tableName = 'table.csv'
+
+if os.path.isfile(tableName):
+	fullNames, themes = initDictionaries(tableName)
 
 for file in files:
 	name, documentType = fileInfo(file)
 	student = find(lambda student: student.name == name, students)
 	if student == None:
-		student = Student(name)
+		fullName = getValueFromDictionary(fullNames, name)
+		theme = getValueFromDictionary(themes, name)
+
+		student = Student(name, fullName, theme)
 		students.append(student)
 	student.updateDocument(file, documentType)
 
